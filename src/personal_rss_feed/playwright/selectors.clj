285daed5eab8f6fc5af1b6f138d26fe629c9e990
J Atkin <16200906+JJ-Atkinson@users.noreply.github.com>
2023-07-30 19:56:46 -0500

    init

---
+++ src/personal_rss_feed/playwright/selectors.clj
@@ -0,0 +1,93 @@
+(ns personal-rss-feed.playwright.selectors
+  (:require
+   [tilley.modules.htmx.form.ui.core :as form.ui]
+   [wally.main :as w]
+   [wally.selectors])
+  (:import
+   (java.lang.reflect Method)
+   (com.microsoft.playwright.impl LocatorUtils)
+   (com.microsoft.playwright Locator Locator$GetByTextOptions Locator$GetByLabelOptions ElementHandle)))
+
+(def
+  ^{:private true
+    :doc "Regain access to the standard selectors hidden by object methods in Playwright. These methods are helpers
+          used by `playwright.impl.PageImpl` to implement the standard selectors behind java functions in a non composable
+          way."}
+  locator-utils-methods
+  (->> (.getDeclaredMethods LocatorUtils)
+    (seq)
+    (into {} (map (juxt #(.getName %)
+                    (fn [m]
+                      (.setAccessible ^Method m true)
+                      m))))))
+
+(defn- invoke-locator-utils-method
+  "Regain access to the standard selectors hidden by object methods in Playwright"
+  [method-name & args]
+  (.invoke ^Method (get locator-utils-methods method-name)
+    nil
+    (into-array Object args)))
+
+(defn text
+  "Selects an element by text content. See https://playwright.dev/java/docs/locators#locate-by-text
+   Returns a selector"
+  [text & {:keys [exact] :or {exact false}}]
+  (invoke-locator-utils-method "getByTextSelector" text (.setExact (Locator$GetByTextOptions.) exact)))
+
+(defn label
+  "Select a form element by the text of its label. See https://playwright.dev/java/docs/locators#locate-by-label
+   Returns a selector"
+  [text & {:keys [exact] :or {exact false}}]
+  (invoke-locator-utils-method "getByLabelSelector" text (.setExact (Locator$GetByLabelOptions.) exact)))
+
+(defn test-id
+  "Select an element by its `data-testid` attribute. See https://playwright.dev/java/docs/locators#locate-by-test-id
+   Returns a selector"
+  [test-id]
+  (invoke-locator-utils-method "getByTestIdSelector" test-id))
+
+(defn field-id
+  ([field-key entity]
+   (field-id field-key entity ""))
+  ([field-key entity extension]
+   (str "#" (form.ui/field-id field-key entity) extension)))
+
+(defn field-test-id
+  ([field-key entity]
+   (field-id field-key entity ""))
+  ([field-key entity extension]
+   (test-id (str (form.ui/field-id field-key entity) extension))))
+
+(defn query-1
+  "Query that returns just one locator. If more than one match is found, an exception is thrown. If none is found,
+   the query is retried until the timeout is reached (usually 5s)
+
+   https://www.javadoc.io/static/com.microsoft.playwright/playwright/1.33.0/com/microsoft/playwright/Locator.html"
+  ^Locator
+  [q]
+  (w/-query q))
+
+(defn query
+  "Returns a seqable locator for the given selector. If none is found, the query is retried until the timeout is 
+   reached (usually 5s), and an exception is thrown.
+
+   https://www.javadoc.io/static/com.microsoft.playwright/playwright/1.33.0/com/microsoft/playwright/Locator.html"
+  [q]
+  (w/query q))
+
+(defn query-1-now
+  "Returns the first element found by the given selector immediately. Unlike `query`, this does not throw if more than one
+   match is found. Nil if none are found.
+
+  https://www.javadoc.io/static/com.microsoft.playwright/playwright/1.33.0/com/microsoft/playwright/ElementHandle.html"
+  ^ElementHandle
+  [q]
+  (.querySelector w/*page* (w/query->selector q)))
+
+(defn query-now
+  "Returns all the elements found by the given selector. Nil if none are found.
+
+  https://www.javadoc.io/static/com.microsoft.playwright/playwright/1.33.0/com/microsoft/playwright/ElementHandle.html"
+  [q]
+  (seq (.querySelectorAll w/*page* (w/query->selector q))))
+
---
+++ src/personal_rss_feed/playwright/wally_utils.clj
@@ -0,0 +1,105 @@
+(ns personal-rss-feed.playwright.wally-utils
+  (:require
+   [tilley.test-system.wally-state :as wally-state]
+   [wally.main :as w])
+  (:import
+   (com.microsoft.playwright BrowserType$LaunchOptions)
+   (com.microsoft.playwright.options Cookie)))
+
+(defn fresh-browser
+  "This is a very expensive call. Avoid using it.
+
+   :headless?            If true, the browser will be headless.
+   :type                 The type of browser to launch. One of #{:chromium :firefox :webkit}
+   :between-action-delay The ms delay between browser actions. Defaults to 0. See LaunchOptions.setSlowMo.
+   :devtools?            Start chromium devtools?"
+  [{:keys [headless? type between-action-delay devtools?]
+    :or {headless? true
+         type :chromium
+         between-action-delay 0
+         devtools? false}}]
+  (let [pw @wally-state/playwright-instance
+        browser (case type
+                  :chromium (.chromium pw)
+                  :firefox (.firefox pw)
+                  :webkit (.webkit pw))]
+    (.launch browser (-> (BrowserType$LaunchOptions.)
+                       (.setHeadless headless?)
+                       (.setSlowMo between-action-delay)
+                       (.setDevtools devtools?)))))
+
+(defn cached-browser
+  [key fallback-create-fn]
+  (fn [] (-> (swap! wally-state/name->playwright-object
+               #(update % key (fn [browser] (or (when (some-> browser (.isConnected)) browser)
+                                              (fallback-create-fn)))))
+           (get key))))
+
+(def default-browser
+  (cached-browser ::default-browser #(fresh-browser {})))
+
+(defn debug-browser
+  [opts]
+  (let [opts (merge {:headless? false
+                     :between-action-delay 75
+                     :devtools? true}
+               opts)]
+    ((cached-browser [::debug-browser opts] #(fresh-browser opts)))))
+
+(defn fresh-browser-context
+  "Semi expensive call. Not usually required, since you can clear & sign in the content of a context with `sign-in-context!`.
+   Mostly useful if you wanted to test multiple users signed in at once."
+  [{:keys [browser
+           default-timeout]
+    :or {browser default-browser
+         default-timeout 5000}}]
+  (let [context (.newContext browser)]
+    (.setDefaultTimeout context default-timeout)
+    context))
+
+(defn sign-in-context!
+  [context user-id]
+  (.clearCookies context)
+  (.clearPermissions context)
+  (when user-id
+    (.addCookies context [(-> (Cookie. "access-token" (str user-id))
+                            (.setDomain "localhost")
+                            (.setPath "/"))])))
+
+(defn fresh-page
+  "Create a playwright page, for use with `w/with-page`.
+
+   :browser-context If supplied, this context will be used. The auth token will be added to this context if supplied.
+   :user-id         If supplied, the page will have appropriate cookies to be signed in as 
+                    user-id **IFF** the `playwright-simple-auth-replacements` are used.
+   :debug           A map which, if present, builds a browser with good defaults for viewing the test live.
+                    Any options are overrides to `fresh-browser`.
+
+   If no browser context is supplied, remaining args are passed to `fresh-browser-context`
+
+   Returns:
+   :page            The page object
+                    https://www.javadoc.io/static/com.microsoft.playwright/playwright/1.33.0/com/microsoft/playwright/Page.html
+   :browser-context The browser context object 
+                    https://www.javadoc.io/doc/com.microsoft.playwright/playwright/latest/com/microsoft/playwright/BrowserContext.html"
+  [{:keys [user-id browser-context debug]}]
+  (let [ctx (or browser-context
+              (fresh-browser-context {:browser (if debug
+                                                 (debug-browser debug)
+                                                 (default-browser))}))]
+    (sign-in-context! ctx user-id)
+    {:page (.newPage ctx)
+     :browser-context ctx}))
+
+(defmacro with-page
+  "Accepts a map of :page and :browser-context, see `fresh-page`"
+  [page-desc & body]
+  `(let [page-desc# ~page-desc]
+     (with-open [page# (:page page-desc#)
+                 browser-context# (:browser-context page-desc#)]
+       (w/with-page page# ~@body))))
+
+(defn navigate
+  [system path]
+  (w/navigate (str "http://localhost:" (:server-port system) path)))
+
